08ef76c599e82e0d280ac400f3b1b477baab1f39
Myles Fung <mylesfung@gmail.com>
2023-06-21 00:50:54 -0700

    importing as a React component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { ReactComponent as Logo } from "./logo.svg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Posts Sharer</h1>
+        <Logo className="App-logo" alt="logo" style={{ height: 300 }}/>
+        <h1>desire, i want to turn into you</h1>
       </header>
     </div>
   );
 }
+
+// <img src="/logo.svg" className="App-logo" alt="logo" />
 
 export default App;
